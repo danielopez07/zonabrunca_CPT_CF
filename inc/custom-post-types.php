1233fe25a1eb95798fd7e996f34beef52e06b22c
Daniel Lopez <danielopez07@gmail.com>
2020-02-29 15:04:21 -0300

    CPT: Cosas que hacer, donde comer, donde alojarse, serv adicionales

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -1,7 +1,7 @@
 <?php
 add_action( 'init', 'brunca_zona_init' );
 /**
- * Register a Zona post type.
+ * Register Zona post type.
  *
  * @link http://codex.wordpress.org/Function_Reference/register_post_type
  */
@@ -48,7 +48,7 @@ function brunca_zona_init() {
 
 add_action( 'init', 'brunca_negocio_init' );
 /**
- * Register a Negocio post type.
+ * Register Negocio post type.
  *
  * @link http://codex.wordpress.org/Function_Reference/register_post_type
  */
@@ -89,4 +89,192 @@ function brunca_negocio_init() {
 	);
 
 	register_post_type( 'negocio', $args );
+}
+
+
+
+add_action( 'init', 'brunca_cosas_que_hacer_init' );
+/**
+ * Register Cosas que Hacer post type.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+function brunca_cosas_que_hacer_init() {
+	$labels = array(
+		'name'               => _x( 'Cosas que hacer', 'post type general name', 'zonabrunca-cpt-cf' ),
+		'singular_name'      => _x( 'Cosas que hacer', 'post type singular name', 'zonabrunca-cpt-cf' ),
+		'menu_name'          => _x( 'Cosas que hacer', 'menu admin', 'zonabrunca-cpt-cf' ),
+		'name_admin_bar'     => _x( 'Cosas que hacer', 'adicionar nuevo en la barra de admin', 'zonabrunca-cpt-cf' ),
+		'add_new'            => _x( 'Adicionar', '', 'zonabrunca-cpt-cf' ),
+		'add_new_item'       => __( 'Adicionar nuevo', 'zonabrunca-cpt-cf' ),
+		'new_item'           => __( 'Nuevo item', 'zonabrunca-cpt-cf' ),
+		'edit_item'          => __( 'Editar item', 'zonabrunca-cpt-cf' ),
+		'view_item'          => __( 'Ver item', 'zonabrunca-cpt-cf' ),
+		'all_items'          => __( 'Todos los items', 'zonabrunca-cpt-cf' ),
+		'search_items'       => __( 'Buscar items', 'zonabrunca-cpt-cf' ),
+		'parent_item_colon'  => __( 'Item padre:', 'zonabrunca-cpt-cf' ),
+		'not_found'          => __( 'No se encontraron Cosas que hacer.', 'zonabrunca-cpt-cf' ),
+		'not_found_in_trash' => __( 'No se encontraron cosas que hacer en la papelera.', 'zonabrunca-cpt-cf' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'description'        => __( 'Descripción.', 'zonabrunca-cpt-cf' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+    'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'cosas-que-hacer' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+    'taxonomies'         => array( 'category', 'post_tag' ),
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
+	register_post_type( 'cosas-que-hacer', $args );
+}
+
+
+
+add_action( 'init', 'brunca_donde_comer_init' );
+/**
+ * Register Donde Comer post type.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+function brunca_donde_comer_init() {
+	$labels = array(
+		'name'               => _x( 'Donde comer', 'post type general name', 'zonabrunca-cpt-cf' ),
+		'singular_name'      => _x( 'Donde comer', 'post type singular name', 'zonabrunca-cpt-cf' ),
+		'menu_name'          => _x( 'Donde comer', 'menu admin', 'zonabrunca-cpt-cf' ),
+		'name_admin_bar'     => _x( 'Donde comer', 'adicionar nuevo en la barra de admin', 'zonabrunca-cpt-cf' ),
+		'add_new'            => _x( 'Adicionar', 'iten', 'zonabrunca-cpt-cf' ),
+		'add_new_item'       => __( 'Adicionar nuevo', 'zonabrunca-cpt-cf' ),
+		'new_item'           => __( 'Nuevo item', 'zonabrunca-cpt-cf' ),
+		'edit_item'          => __( 'Editar item', 'zonabrunca-cpt-cf' ),
+		'view_item'          => __( 'Ver item', 'zonabrunca-cpt-cf' ),
+		'all_items'          => __( 'Todos los items', 'zonabrunca-cpt-cf' ),
+		'search_items'       => __( 'Buscar item', 'zonabrunca-cpt-cf' ),
+		'parent_item_colon'  => __( 'Item padre:', 'zonabrunca-cpt-cf' ),
+		'not_found'          => __( 'No se encontraron Donde Comer.', 'zonabrunca-cpt-cf' ),
+		'not_found_in_trash' => __( 'No se encontraron Donde comer en la papelera.', 'zonabrunca-cpt-cf' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'description'        => __( 'Descripción.', 'zonabrunca-cpt-cf' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+    'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'donde-comer' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+    'taxonomies'         => array( 'category', 'post_tag' ),
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
+	register_post_type( 'donde-comer', $args );
+}
+
+
+
+add_action( 'init', 'brunca_donde_alojarse_init' );
+/**
+ * Register Donde Alojarse post type.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+function brunca_donde_alojarse_init() {
+	$labels = array(
+		'name'               => _x( 'Donde Alojarse', 'post type general name', 'zonabrunca-cpt-cf' ),
+		'singular_name'      => _x( 'Donde Alojarse', 'post type singular name', 'zonabrunca-cpt-cf' ),
+		'menu_name'          => _x( 'Donde Alojarse', 'menu admin', 'zonabrunca-cpt-cf' ),
+		'name_admin_bar'     => _x( 'Donde Alojarse', 'adicionar nuevo en la barra de admin', 'zonabrunca-cpt-cf' ),
+		'add_new'            => _x( 'Adicionar', 'donde alojarse', 'zonabrunca-cpt-cf' ),
+		'add_new_item'       => __( 'Adicionar nuevo', 'zonabrunca-cpt-cf' ),
+		'new_item'           => __( 'Nuevo item', 'zonabrunca-cpt-cf' ),
+		'edit_item'          => __( 'Editar item', 'zonabrunca-cpt-cf' ),
+		'view_item'          => __( 'Ver item', 'zonabrunca-cpt-cf' ),
+		'all_items'          => __( 'Todos los items', 'zonabrunca-cpt-cf' ),
+		'search_items'       => __( 'Buscar items', 'zonabrunca-cpt-cf' ),
+		'parent_item_colon'  => __( 'Negocio padre:', 'zonabrunca-cpt-cf' ),
+		'not_found'          => __( 'No se encontraron Donde Alojarse.', 'zonabrunca-cpt-cf' ),
+		'not_found_in_trash' => __( 'No se encontraron Donde Alojarse en la papelera.', 'zonabrunca-cpt-cf' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'description'        => __( 'Descripción.', 'zonabrunca-cpt-cf' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+    'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'donde-alojarse' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+    'taxonomies'         => array( 'category', 'post_tag' ),
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
+	register_post_type( 'donde-alojarse', $args );
+}
+
+
+
+add_action( 'init', 'brunca_servicios_adicionales_init' );
+/**
+ * Register Servicios adicionales post type.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+function brunca_servicios_adicionales_init() {
+	$labels = array(
+		'name'               => _x( 'Servicios Adicionales', 'post type general name', 'zonabrunca-cpt-cf' ),
+		'singular_name'      => _x( 'Servicios Adicionales', 'post type singular name', 'zonabrunca-cpt-cf' ),
+		'menu_name'          => _x( 'Servicios Adicionales', 'menu admin', 'zonabrunca-cpt-cf' ),
+		'name_admin_bar'     => _x( 'Servicios Adicionales', 'adicionar nuevo en la barra de admin', 'zonabrunca-cpt-cf' ),
+		'add_new'            => _x( 'Adicionar', 'Servicios Adicionales', 'zonabrunca-cpt-cf' ),
+		'add_new_item'       => __( 'Adicionar nuevo', 'zonabrunca-cpt-cf' ),
+		'new_item'           => __( 'Nuevo item', 'zonabrunca-cpt-cf' ),
+		'edit_item'          => __( 'Editar item', 'zonabrunca-cpt-cf' ),
+		'view_item'          => __( 'Ver item', 'zonabrunca-cpt-cf' ),
+		'all_items'          => __( 'Todos los items', 'zonabrunca-cpt-cf' ),
+		'search_items'       => __( 'Buscar items', 'zonabrunca-cpt-cf' ),
+		'parent_item_colon'  => __( 'Item padre:', 'zonabrunca-cpt-cf' ),
+		'not_found'          => __( 'No se encontraron Servicios Adicionales.', 'zonabrunca-cpt-cf' ),
+		'not_found_in_trash' => __( 'No se encontraron Servicios Adicionales en la papelera.', 'zonabrunca-cpt-cf' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'description'        => __( 'Descripción.', 'zonabrunca-cpt-cf' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+    'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'servicios-adicionales' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+    'taxonomies'         => array( 'category', 'post_tag' ),
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
+	register_post_type( 'servicios', $args );
 }
